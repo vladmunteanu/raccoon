a6242ec2f64ad87dc495143514e525319b556fc6
Vitalie Maldur <vitalie.maldur@avira.com>
2015-11-13 16:34:42 +0200

    Minor refactoring audit log model

--- src/models/auditlog.py
+++ src/models/auditlog.py
@@ -11,14 +11,3 @@ class AuditLog(Document):
     environment = StringField(required=True)
     project = StringField(required=True)
     date_added = DateTimeField(required=True, auto_now_on_insert=True)
-
-
-{
-  "id": "unique",
-  "message": "log message",
-  "user": "user name",
-  "action": "action name",
-  "environment": "env name",
-  "project": "project Name",
-  "date_added": "12-12-2012"
-}
