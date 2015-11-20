dce2835e28d45f4a1fc599e75cab7fb12134ad38
Alexandru Mihai <92.alexandru.mihai@gmail.com>
2015-11-20 11:48:33 +0200

    Moved server and setting in root. Moved all settings vars in settings.py

--- server.py
+++ server.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python
+
 from __future__ import absolute_import
 
 import logging
 import os
 
+from motorengine.connection import connect
+import tornado
 import tornado.ioloop
 import tornado.options
 import tornado.web
+
+from settings import DB, APP
 
 
 log = logging.getLogger(__name__)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('../../web/index.html', title='Raccoon - Deployment Tool', cdn='web')
+        self.render('index.html', title='Raccoon - Deployment Tool', cdn='web')
 
 class PingHandler(tornado.web.RequestHandler):
     def get(self):
@@ -23,18 +29,12 @@ class Application(tornado.web.Application):
     Main Application
     """
     def __init__(self):
-        settings = {
-            'static_path': os.path.join(os.path.dirname(__file__), '../../web'),
-            'debug': True,
-        }
-
         handlers = [
             (r'/', MainHandler),
             (r'/ping', PingHandler),
-            (r'/static/(.*)', tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
+            (r'/static/(.*)', tornado.web.StaticFileHandler, dict(path=APP['static_path'])),
         ]
-
-        tornado.web.Application.__init__(self, handlers, **settings)
+        tornado.web.Application.__init__(self, handlers, **APP)
 
         # show requests in stdout
         tornado.options.parse_command_line()
@@ -42,6 +42,11 @@ class Application(tornado.web.Application):
 def main():
     app = Application()
     app.listen(8888)
+
+    # Connect to Mongo Server
+    io_loop = tornado.ioloop.IOLoop.instance()
+    connect(DB['name'], host=DB['host'], port=DB['port'], io_loop=io_loop)
+
     tornado.ioloop.IOLoop.current().start()
 
 
---
+++ settings.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+import os
+
+
+ROOT_PATH = os.path.dirname(__file__)
+
+DB = {
+    'host': '10.2.1.77',
+    'port': 27017,
+    'name': 'test',
+}
+
+DEBUG = True
+STATIC_PATH = os.path.join(ROOT_PATH, 'web')
+TEMPLATE_PATH = os.path.join(ROOT_PATH, 'web')
+
+APP = {
+    'static_path': STATIC_PATH,
+    'template_path': TEMPLATE_PATH,
+    'debug': DEBUG,
+}
