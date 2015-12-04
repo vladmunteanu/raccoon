bb99d62aae969e80287bd1ba24207955b747724b
Vitalie Maldur <vitalie.maldur@avira.com>
2015-12-04 13:00:19 +0200

    Date added field as timestamp

--- src/raccoon/utils/utils.py
+++ src/raccoon/utils/utils.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+import time
+
 from bson.objectid import ObjectId
 
 
 def json_serial(obj):
     if isinstance(obj, datetime):
-        return obj.isoformat()
+        return int(time.mktime(obj.timetuple()))
 
     if isinstance(obj, ObjectId):
         return str(obj)
