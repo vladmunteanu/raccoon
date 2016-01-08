e66b800d9408a130819c95e68314dd8e53a6adb5
Vitalie Maldur <vitalie.maldur@avira.com>
2016-01-08 18:19:21 +0200

    Modified project component to handler modification

--- web/js/components/settings/Project.react.js
+++ web/js/components/settings/Project.react.js
@@ -8,7 +8,9 @@ var Project = React.createClass({
         let project = ProjectStore.getById(this.props.params.id);
         return {
             project: project,
-            authType: "basic"
+            details: {
+                authType: "basic"
+            }
         };
     },
 
@@ -28,51 +30,95 @@ var Project = React.createClass({
     },
 
     _onChangeAuthType: function(event) {
-        let isBasicAuth = event.target.value === "basic";
+        let details = this.state.details;
+        details.authType = event.target.value;
         this.setState({
-            authType: isBasicAuth ? "basic" : "oauth"
+            details: details
         });
     },
 
+    _onChangeName: function(event) {
+        this.state.project.name = event.target.value;
+        this.setState({
+            name: event.target.value
+        });
+    },
+
+    _onChangeUsername: function(event) {
+        let details = this.state.details;
+        details.username = event.target.value;
+        this.setState({
+            details: details
+        });
+    },
+
+    _onChangePassword: function(event) {
+        let details = this.state.details;
+        details.password = event.target.value;
+        this.setState({
+            details: details
+        });
+    },
+
+    _onChangeToken: function(event) {
+        let details = this.state.details;
+        details.token = event.target.value;
+        this.setState({
+            details: details
+        });
+    },
+
+    onSubmit: function (event) {
+        event.preventDefault();
+        console.log({
+            name: !!this.state.name ? this.state.name : this.state.project.name,
+            details: this.state.details
+        });
+        //AppDispatcher.dispatch({
+        //    action: ActionTypes.REGISTER_USER,
+        //    data: this.state
+        //});
+    },
+
     render: function () {
-        let project = ProjectStore.getById(this.props.params.id);
+        this.state.project = ProjectStore.getById(this.props.params.id);
         let githubCredentialsForm = (
             <div>
                 <div className="form-group">
                     <label htmlFor="username" className="control-label">Github username</label>
-                    <input type="text" className="form-control"
+                    <input type="text" className="form-control" onChange={this._onChangeUsername}
                            id="username" placeholder="Username"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password" className="control-label">Github password</label>
-                    <input type="password" className="form-control"
+                    <input type="password" className="form-control" onChange={this._onChangePassword}
                            id="password" placeholder="Password"/>
                 </div>
             </div>
         );
 
-        if(this.state.authType === "oauth")
+        if(this.state.details.authType === "oauth")
             githubCredentialsForm = (
                 <div className="form-group">
                     <label htmlFor="token" className="control-label">Github token</label>
-                    <input type="text" className="form-control"
+                    <input type="text" className="form-control" onChange={this._onChangeToken}
                            id="token" placeholder="Token"/>
                 </div>
             );
 
-        if(!!project)
+        if(!!this.state.project)
             return (
                 <div className="container">
                     <h3>Project details</h3>
-                    <form className="form-horizontal col-sm-4">
+                    <form onSubmit={this.onSubmit} className="form-horizontal col-sm-4">
                         <div className="form-group">
                             <label htmlFor="project-name" className="control-label">Project name</label>
-                            <input type="text"  className="form-control"
-                                   id="project-name" value={project.name} placeholder="Project Name"/>
+                            <input type="text"  className="form-control" onChange={this._onChangeName}
+                                   id="project-name" value={this.state.project.name} placeholder="Project Name"/>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="git-auth" className="control-label">Git authentic ation method</label><br/>
-                            <select className="form-control" id="git-auth" ref="auth-method" onChange={this._onChangeAuthType}>
+                            <label htmlFor="git-auth" className="control-label">Git authentication method</label><br/>
+                            <select className="form-control" id="git-auth" onChange={this._onChangeAuthType}>
                                 <option value="basic">Basic</option>
                                 <option value="oauth">OAuth</option>
                             </select>
