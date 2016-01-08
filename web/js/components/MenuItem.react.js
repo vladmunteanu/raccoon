8eba9c74f065c4da11d75c3d0785a9ff66ad6213
Alexandru Mihai <92.alexandru.mihai@gmail.com>
2016-01-08 13:43:11 +0200

    Created left side submenu

--- web/js/components/MenuItem.react.js
+++ web/js/components/MenuItem.react.js
@@ -23,6 +23,20 @@ class MenuItem extends React.Component {
         });
     }
 
+    handleMouseEnter(event) {
+        var position = event.target.getBoundingClientRect();
+        var dropdownMenu = $(event.target).parent().find('.dropdown-menu');
+        var top =
+            position.top + dropdownMenu.height() <= $(document).height() ?
+                position.top :
+                position.top - dropdownMenu.height() + 25;
+
+        dropdownMenu.css({
+            top: top + 'px',
+            left: position.width + 'px'
+        });
+    }
+
     render() {
         var id = 'onoffswitch-' + this.props.item.id;
         var checked = this.state.checked;
@@ -30,17 +44,29 @@ class MenuItem extends React.Component {
             <div className="onoffswitch pull-right">
                 <input type="checkbox" name="onoffswitch"
                        className="onoffswitch-checkbox" id={id} checked={checked}
-                       onChange={this.handleChange.bind(this)}/>
+                       onChange={this.handleChange.bind(this)} />
                 <label className="onoffswitch-label" htmlFor={id}/>
             </div>
         ) : '';
 
         return (
-            <li>
-                <a href="#">
+            <li className="dropdown">
+                <a href="javascript: void(0);"
+                   onMouseEnter={this.handleMouseEnter.bind(this)}
+                   className="dropdown-toggle"
+                   aria-haspopup="true"
+                   aria-expanded="false"
+                >
                     {this.props.item.label || this.props.item.name}
                     {rightSwitch}
                 </a>
+                <ul className="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" className="divider" />
+                    <li><a href="#">Separated link</a></li>
+                </ul>
             </li>
         );
     }
