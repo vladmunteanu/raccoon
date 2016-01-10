aef285ddbe0f4f2d0e002ceabe0d83f16e658251
Vitalie Maldur <vitalie.maldur@avira.com>
2016-01-10 17:52:25 +0200

    Added details field to projects

--- src/raccoon/models/project.py
+++ src/raccoon/models/project.py
@@ -1,12 +1,27 @@
 from __future__ import absolute_import
 
-from motorengine import StringField, DateTimeField
+from motorengine import StringField, DateTimeField, BaseField
 from raccoon.models import BaseModel
+
+class DictField(BaseField):
+    """
+    Field responsible for storing dict objects.
+    """
+
+    def validate(self, value):
+        return isinstance(value, dict)
+
+    def to_son(self, value):
+        return value
+
+    def from_son(self, value):
+        return value
 
 
 class Project(BaseModel):
     __collection__ = 'projects'
 
     name = StringField(required=True, unique=True)
+    details = DictField(default={})
     date_added = DateTimeField(required=True, auto_now_on_insert=True)
 
