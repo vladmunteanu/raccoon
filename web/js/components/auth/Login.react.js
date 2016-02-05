4e2a54da92f85d4f008d67b3abe44627fd9ecaa6
Vitalie Maldur <vitalie.maldur@avira.com>
2016-02-05 16:37:43 +0200

    Frontend validation for login form

--- web/js/components/auth/Login.react.js
+++ web/js/components/auth/Login.react.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { History } from 'react-router';
+import classnames from 'classnames';
+import Joi from 'joi';
+import strategy from 'joi-validation-strategy';
+import validation from 'react-validation-mixin';
 
 import RaccoonApp from '../RaccoonApp.react';
 
@@ -28,12 +32,35 @@ let Login = React.createClass({
         return getLocalState();
     },
 
+    validatorTypes: {
+        username: Joi.string().required().label('Username'),
+        password: Joi.string().alphanum().min(8).max(30).required().label('Password')
+    },
+
+    getValidatorData: function () {
+        return this.state;
+    },
+
+    renderHelpText: function (messages) {
+        return (
+            <div className="text-danger">
+                {
+                    messages.map(message => {
+                        return <div>{message}</div>
+                    })
+                }
+            </div>
+        );
+    },
+
     login: function (event) {
         event.preventDefault();
-        AppDispatcher.dispatch({
-            action: ActionTypes.LOGIN_USER,
-            data: this.state,
-        });
+        if(this.props.isValid()) {
+            AppDispatcher.dispatch({
+                action: ActionTypes.LOGIN_USER,
+                data: this.state,
+            });
+        }
     },
 
     componentDidMount: function() {
@@ -76,7 +103,6 @@ let Login = React.createClass({
                 </div>
             );
         }
-
         return (
 
             <div className="row">
@@ -89,16 +115,22 @@ let Login = React.createClass({
                         <h3>Sign In</h3>
                         <form onSubmit={this.login} className="form-horizontal col-sm-4">
                             <div className="form-group">
-                                <label htmlFor="username" className="control-label">Username or Email</label>
-                                <input type="text" value={this.state.username} className="form-control"
-                                       onChange={this._onUsernameChange}
-                                       id="username" placeholder="Username or Email"/>
+                                <label htmlFor="username" className="control-label">Username</label>
+                                <input type="text" ref="username" id="username"
+                                    className="form-control" placeholder="Username"
+                                    value={this.state.username}
+                                    onChange={this._onUsernameChange}
+                                    onBlur={this.props.handleValidation('username')}/>
+                                {this.renderHelpText(this.props.getValidationMessages('username'))}
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password" className="control-label">Password</label>
-                                <input type="password" value={this.state.password} className="form-control"
-                                       onChange={this._onPasswordChange}
-                                       id="password" placeholder="Password"/>
+                                <input type="password" ref="password" id="password"
+                                    className="form-control" placeholder="Password"
+                                    value={this.state.password}
+                                    onChange={this._onPasswordChange}
+                                    onBlur={this.props.handleValidation('password')}/>
+                                {this.renderHelpText(this.props.getValidationMessages('password'))}
                             </div>
                             <div className="form-group">
                                 <a href="/#/register">Not registered yet?</a>
@@ -109,7 +141,8 @@ let Login = React.createClass({
                 </div>
             </div>
         );
+
     }
 });
 
-export default Login;
+export default validation(strategy)(Login);
