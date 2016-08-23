30881433c2b6dd92b1df7e6d3697fe784d8ee821
Vlad Stefan Munteanu <vlad.munteanu@avira.com>
2016-08-23 10:15:41 +0300

    Reordered raccoon import

--- src/raccoon/controllers/auth.py
+++ src/raccoon/controllers/auth.py
@@ -4,8 +4,8 @@ import logging
 
 import jwt
 from tornado import gen
-from settings import SECRET
 
+from settings import SECRET
 from .base import BaseController
 from ..models import User
 from ..utils.exceptions import ReplyError
