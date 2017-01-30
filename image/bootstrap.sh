d57f090032ab2794b5de73d4a7f13c9f58e9f9bf
Mihai Danilescu <mihai.danilescu@avira.com>
2017-01-30 14:16:10 +0200

    added vagrant support

---
+++ image/bootstrap.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# setting vars
+PATH_VENV=/opt/venv
+PATH_RACCOON=/opt/raccoon
+PATH_SRC=${PATH_RACCOON}/src
+PATH_WEB=${PATH_RACCOON}/web
+
+# redirect all output to /tmp/bootstrap.log
+&>/tmp/bootstrap.log
+
+# initial setup
+sudo apt-get --yes --allow-change-held-packages install python3
+sudo apt-get --yes --allow-change-held-packages install python3-pip
+sudo apt-get --yes --allow-change-held-packages install mongodb
+sudo apt-get --yes --allow-change-held-packages install npm
+
+export LC_ALL=C
+pip3 install virtualenv
+
+sudo mkdir ${PATH_VENV}
+sudo chown ubuntu ${PATH_VENV}
+
+# create virtual env
+virtualenv ${PATH_VENV}
+
+# setup virtual env
+echo "export PYTHONPATH=\$PYTHONPATH:${PATH_SRC}">>${PATH_VENV}/bin/activate
+ln -s ${PATH_RACCOON}/sys/bin/raccoon ${PATH_VENV}/bin/raccoon
+ln -s ${PATH_RACCOON}/sys/bin/raccoonshell ${PATH_VENV}/bin/raccoonshell
+
+source ${PATH_VENV}/bin/activate
+
+# setup project
+cd ${PATH_RACCOON}
+pip install -r requirements.txt
+cd ${PATH_WEB}
+npm install
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo npm install -g watchify
+sudo npm install -g browserify
+
+# start raccoon
+npm start
+
